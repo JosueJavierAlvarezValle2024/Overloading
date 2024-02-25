56a75e79b981ee6412c932bc18e161d28d14273e
Jjav2 <Jjav2@HPCOMPUTER18>
2024-02-24 20:17:17 -0600

    Agregar archivos de proyecto.

---
+++ Overloading/Program.cs
@@ -0,0 +1,23 @@
+﻿using Overloading;
+
+Sum_of_decimal_integers sum = new Sum_of_decimal_integers();
+
+// Here the use of overloads will be made
+
+int _result1 = sum.Sum(35,65); // Sum of 2 integer quantities
+
+int _result2 = sum.Sum(100,200,300); // Sum of 3 integer quantities
+
+double _result3 = sum.Sum(25.5, 35.5); // Sum of 2 decimal places
+
+double _result4 = sum.Sum(8.6,1.6,5.7,8.6); // Sum of 4 decimal places
+
+// Results
+Console.WriteLine("Result 1 is :" +_result1);
+Console.WriteLine("-----------------------------------");
+Console.WriteLine("Result 2 is :" + _result2);
+Console.WriteLine("-----------------------------------");
+Console.WriteLine("Result 3 is :" + _result3);
+Console.WriteLine("-----------------------------------");
+Console.WriteLine("Result 4 is :" + _result4);
+Console.ReadLine();
---
+++ Overloading/Sum_of_decimal_integers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    public class Sum_of_decimal_integers
+    {
+        public int Sum (int j, int k) // Here is the sum of 2 integers
+        {
+            return j + k;
+        }
+
+        public int Sum (int x, int y, int z) // Here is the sum of 3 integers
+        {
+            return x + y + z;
+        }
+
+        public double Sum (double m, double n) // Here is the sum of 2 decimal numbers
+        { 
+            return m + n; 
+        }
+        public double Sum (double a, double b, double c, double d) // Here is the sum of 4 decimal numbers
+        { 
+            return a + b + c + d; 
+        }
+    }
+}
